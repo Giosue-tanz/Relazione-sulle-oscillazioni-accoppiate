b027d4c4b6983dcf36a4298c5ed02be81e970099
Giosue-tanz <120932444+Giosue-tanz@users.noreply.github.com>
2024-03-12 18:22:31 +0100

    Add files via upload

--- oscillazioni_forzate.py
+++ oscillazioni_forzate.py
@@ -1,4 +1,12 @@
 import numpy as np
 from scipy.optimize import curve_fit
 
-data = np.loadtxt("dati.txt", unpack=True)
+path = "./misurazioni/"
+
+data = np.loadtxt(path+"misurazione_senza_smorzatore_solo.txt", unpack="True")
+osc_s_smorz = {"tA": [], "posA": [], "tB": [], "posB": []}
+
+osc_s_smorz["tA"] = data[0]
+osc_s_smorz["posA"] = data[1]
+osc_s_smorz["tB"] = data[2]
+osc_s_smorz["posB"] = data[3]
